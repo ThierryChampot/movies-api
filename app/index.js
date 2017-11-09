be96e02ad748e5e84e01f573b00774db71df4e54
ThierryChampot <thierry.champot@gmail.com>
2017-11-09 16:17:29 +0100

    prepare to merge

--- app/index.js
+++ app/index.js
@@ -13,9 +13,34 @@ const bodyParser = require('body-parser');
 // importer les routeurs
 const moviesRouter = require('./routers/moviesRouter');
 
+// importer la connexion à la base de données
+const db = require('node-mysql');
+const DB = db.DB;
+const BaseRow = db.Row;
+const BaseTable = db.Table;
+
+// // se connecter à la base de données
+const config = require('../config/config');
+const box = new DB(config.database);
+
+// // tester la connexion à la base de données
+// var basicTest = function(cb) {
+//     box.connect(function(conn, cb) {
+//         cps.seq([
+//             function(_, cb) {
+//                 conn.query('select * from genre', cb);
+//             },
+//             function(res, cb) {
+//                 console.log(res);
+//                 cb();
+//             }
+//         ], cb);
+//     }, cb);
+// };
+
+
 // attribuer les routes aux routeurs
 app.use('/movies', moviesRouter);
 
 // lancement du serveur
 app.listen(port, () => console.log(`TEST Server running at http://127.0.0.1:${port}`));
-
