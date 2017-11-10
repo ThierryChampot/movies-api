5143d14c65aa72d0e6a9cdf44e63449b4f8606fd
Sarah <sarah.drapeau@hotmail.fr>
2017-11-10 12:48:15 +0100

    rebase dev

--- app/index.js
+++ app/index.js
@@ -13,21 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // import de body-parser
 const bodyParser = require('body-parser');
-
-// importer les routeurs
-const moviesRouter = require('./routers/moviesRouter');
-const genresRouter = require('./routers/genresRouter');
-const peopleRouter = require('./routers/peopleRouter');
-const reviewRouter = require('./routers/reviewRouter');
 
 // const mysql      = require('mysql');
 // const database = require('../config/config').database;
@@ -50,6 +44,12 @@ const reviewRouter = require('./routers/reviewRouter');
 //         console.log('Error while performing Query.');
 //       });
 //     });
+
+// importer les routeurs
+const moviesRouter = require('./routers/moviesRouter');
+const genresRouter = require('./routers/genresRouter');
+const peopleRouter = require('./routers/peopleRouter');
+const reviewRouter = require('./routers/reviewRouter');
 
 // attribuer les routes aux routeurs
 app.use('/movies', moviesRouter);
