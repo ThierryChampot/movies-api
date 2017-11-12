a9d98ed222eb9fbef27418a2f5950a712e55d8cf
Timothée <timothee.aupetit@gmail.com>
2017-11-12 16:09:20 +0100

    implemented review creation

--- app/routers/reviewRouter.js
+++ app/routers/reviewRouter.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const ReviewsModel = require('../models/reviewsModel');
-let reviewsModel = new ReviewsModel();
+const connection = require('../dbConnector');
+const ReviewModel = require('../models/reviewModel');
 
+const reviewModel = new ReviewModel(connection);
 
+router.route('/')
+    .post(function(req, res) {
+        const body = req.body;
+        reviewModel.createReview(body, function(err, data) {
+            if(err) {
+                console.log(err)
+            } else {
+                return res.json(data)
+            }
+        });
+    });
 
 module.exports = router;
